fb41e7b1beac77752bee2a02f657ffb4b1cf7322
valrat <leri.ratu@gmail.com>
2018-11-18 23:37:43 -0800

    writing into database

--- icon_farming/icon_farming.js
+++ icon_farming/icon_farming.js
@@ -25,23 +25,44 @@ connection.connect()
 // TODO: get this from command line? 
 var icon_folder = '/home/valerian/Programming/sscaptcha/icons'
 
+/**
+ * Inserts the icon into the database with the proper label
+ * @param {icon} icon 
+ * @param {string} filename 
+ */
+function insert_into_database(icon, filename) {
+    console.log(icon.term)
+    connection.query('insert into icons (label, filename) values ?',
+        [[[icon.term, filename]]],
+        function (err, res, fields) {
+            if (err) {
+                throw err;
+            }
+            return
+        }
+    )
+    return
+}
 
 /**
  * This function downloads an image into the folder
  * @param {string} icon_folder 
  * @param {icon} icon
- * @param {function} cb
  */
-function download_icon(icon_folder, icon, cb) {
+function download_icon(icon_folder, icon) {
     request.head(icon.preview_url, function(err, res, body){
         let filename = icon.term + '-' + icon.id + '.png'
         let full_filepath = icon_folder + '/' + filename
         console.log('Downloading: ' + icon.preview_url)
         console.log('to: ' + full_filepath)
         request(icon.preview_url).pipe(fs.createWriteStream(full_filepath))
-            .on('close', cb)
+            .on('close', function() {
+                insert_into_database(icon, filename)
+            })
     })
+    return
 }
+
 
 var cb_func = function(err, data) {
     if (!err) {
@@ -50,8 +71,9 @@ var cb_func = function(err, data) {
         for (var i = 0; i < num_icons; i++) {
             download_icon(icon_folder, icons[i], function(){})
         }
+    } else {
+        console.log(err)
     }
-    console.log(err)
 }
 
-nounproject.getIconsByTerm('star', {limit: 5}, cb_func)
+nounproject.getIconsByTerm('star', {limit: 2}, cb_func)
