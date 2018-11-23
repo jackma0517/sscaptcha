7bcaa54582696339585a4206a049528a0be518e3
Michelle Yang <michyang8@gmail.com>
2018-11-23 15:10:48 -0800

    survey results send to server

--- public/client.js
+++ public/client.js
@@ -77,7 +77,7 @@ recorder.moveListener();
 /*
  * AJAX Post Request
  */
-function ajaxPost(url, data) {
+function ajaxPost(url, data, onSuccess, onError) {
     var xhttp = new XMLHttpRequest();
     var payload = JSON.stringify(data);
     console.log(payload);
@@ -89,7 +89,9 @@ function ajaxPost(url, data) {
       var status = xhttp.status;
       if (status == 200) { /* Success */
         console.log("POST success");
-        showSurvey();
+        if(onSuccess){
+          onSuccess();
+        }
         //var responseObj = JSON.parse(xhttp.responseText);
         //var message = responseObj.boop;
         //alert(message);
@@ -97,6 +99,9 @@ function ajaxPost(url, data) {
       } else if (status.toString().charAt(0) == 4 || status.toString().charAt(0) == 5) {
         /* Client or network error */
         console.log("POST failure");
+        if(onError){
+          onError();
+        }
       }  
     }
     xhttp.send(payload);
@@ -152,41 +157,85 @@ function ajaxGet(url) {
 //called after all HTML/CSS/Scripts/DOM are loaded
 window.onload = function () {
 
-    //var username = prompt('What\'s your username?');
-    //starts recording on load
-    recorder.record();
+  //starts recording on load
+  recorder.record();
 
-    document.getElementById('submit-btn').addEventListener('click', function(){
-        //socket.emit('message', 'Hi server, how are you?');
-        if(mouseMovementArray.length == 0){
-            alert("Please record mouse movements before submitting captcha");
-        } else {
-            //playback mouse movements to user for better usability?
-            recorder.playback();
-            var data = {
-                mouseMovements: mouseMovementArray,
-                mouseClicks: mouseClicksArray
-            };
-            ajaxPost('http://localhost:8080' + '/submit', data);
-        }
-    })
+  document.getElementById('submit-btn').addEventListener('click', function(){
+    if(mouseMovementArray.length == 0){
+      alert("Please record mouse movements before submitting captcha");
+    } else {
+    //playback mouse movements to user for better usability?
+      recorder.playback();
+      var data = {
+        mouseMovements: mouseMovementArray,
+        mouseClicks: mouseClicksArray
+      };
+        ajaxPost('http://localhost:8080' + '/submit', data, showSurvey(), 
+        function(){
+          alert("Authentication failed please retry captcha");
+        });
+      }
+  })
 
-    document.getElementById('get-captcha').addEventListener('click', function(){
-        console.log('Client requesting CAPTCHA');
-        ajaxGet('http://localhost:8080' + '/captcha');
-    })
+  document.getElementById('get-captcha').addEventListener('click', function(){
+    console.log('Client requesting CAPTCHA');
+    ajaxGet('http://localhost:8080' + '/captcha');
+  })
 
-    document.getElementById("captcha-image").addEventListener('click', function(e){
-        var x = e.pageX - this.offsetLeft;
-        var y = e.pageY - this.offsetTop;
-        mouseClicksArray.push([x,y]);
-    })
+  document.getElementById("captcha-image").addEventListener('click', function(e){
+    var x = e.pageX - this.offsetLeft;
+    var y = e.pageY - this.offsetTop;
+    mouseClicksArray.push([x,y]);
+  })
 
-    document.getElementById("survey-btn").addEventListener('click', function(){
-      showSurvey();
-    })
-    document.getElementById("survey-close-btn").addEventListener('click', function(){
-      hideSurvey();
-    })
-  }
+  document.getElementById("survey-btn").addEventListener('click', function(){
+    showSurvey();
+  })
+  document.getElementById("survey-close-btn").addEventListener('click', function(){
+    hideSurvey();
+  })
+  document.getElementById("survey-submit-btn").addEventListener('click', function(){
+    if( !checkSurveyComplete()){
+      alert("All Questions Must Be Answered To Submit");
+    } else {
+      submitSurvey();
+    }
+  })
+}
 
+function checkSurveyComplete(){
+  if($("input[name=num-attempts]:checked").val() == undefined ||
+     $("input[name=solve-time]:checked").val() == undefined ||
+     $("input[name=solve-again]:checked").val() == undefined ||
+     $("input[name=recognize-shapes]:checked").val() == undefined ||
+     $("input[name=ambiguity-level]:checked").val() == undefined ||
+     $("input[name=difficulty-level]:checked").val() == undefined ||
+     $("input[name=shape-sizes]:checked").val() == undefined ||
+     $("input[name=interactive-difficulty]:checked").val() == undefined){
+      return false;
+  } 
+  return true;
+}
+function submitSurvey(){
+
+  var surveyResults = {
+    num_attempts : $("input[name=num-attempts]:checked").val(),
+    solve_time : $("input[name=solve-time]:checked").val(),
+    solve_again : $("input[name=solve-again]:checked").val(),
+    recognize_shapes : $("input[name=recognize-shapes]:checked").val(),
+    ambiguity_level : $("input[name=ambiguity-level]:checked").val(),
+    difficulty_level : $("input[name=difficulty-level]:checked").val(),
+    shape_sizes : $("input[name=shape-sizes]:checked").val(),
+    interactive_difficulty : $("input[name=interactive-difficulty]:checked").val()
+  };
+
+  var data = {
+    surveyResults: surveyResults
+  };
+
+  ajaxPost('http://localhost:8080' + '/submitSurvey', data, function(){
+    alert("Thank you for participating :) ");
+  }, function(){
+    alert("Error Receiving Survey. Please Submit Again");
+  });
+}
