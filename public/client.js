f18290b91d5cedea96ae096cbde6422479a9916d
Jack MA <jackma@dhcp-128-189-236-75.ubcsecure.wireless.ubc.ca>
2018-11-23 18:04:04 -0800

    adding alert after post

--- public/client.js
+++ public/client.js
@@ -89,6 +89,7 @@ function ajaxPost(url, data, onSuccess, onError) {
       var status = xhttp.status;
       if (status == 200) { /* Success */
         console.log("POST success");
+        console.log("response: " + xhttp.responseText);
         alert(xhttp.responseText);
         if(onSuccess){
           onSuccess();
