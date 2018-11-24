43955d6fdd7525d7d085fa026eaa2f3108100b70
Jack MA <jackma@dhcp-128-189-236-75.ubcsecure.wireless.ubc.ca>
2018-11-23 18:00:42 -0800

    added checker for click

--- solutionChecker.js
+++ solutionChecker.js
@@ -4,20 +4,76 @@
 //      {'action', (coordinates)},
 //      {'action', (coordinates)}
 //     ]
-const pngSize = 200;
+
+// Db Access
+var sqlite3 = require('sqlite3').verbose()
+require('dotenv').config({path:'./.env'})
+var db = new sqlite3.Database(process.env.SQLITE_DB)
 
 
+const pngSize = 100;
 
-function checkMouseClick(solution, mouseClicks){
+function getSolution(solutionID){
+    var sql = 'select solution from captchas where guid = ?';
+
     return new Promise(function(resolve,reject){
-        for(var i = 0; i< solution.length; i++){
-            if(!(mouseClicks[i][0]>solution[i][1][0] && mouseClicks[i][0]<solution[i][1][0]+pngSize)){
-                reject("Click " + i + " doesn't match");
+        db.get(sql, [solutionID], (err, result) => {
+            if (err) {
+                console.log(err)
+                reject(err);
+            } else {
+                resolve(JSON.parse(result.solution));
             }
-            if(!(mouseClicks[i][1]>solution[i][1][1] && mouseClicks[i][1]<solution[i][1][1]+pngSize)){
-                reject("Click " + i + " doesn't match");
-            }
-        }
-        resolve("Clicking good");
+        })
     });
+
+}
+
+
+async function verify(solutionID, mouseClicks, mouseMovement){
+    var solution = await getSolution(solutionID);
+    return new Promise((resolve, reject)=>{
+        if(!checkMouseClick(solution,mouseClicks)){
+            reject("mouse click doesn't match");
+        }
+        if(!checkMouseMovement(mouseMovement)){
+            reject("mouse movement is suspicious");
+        }
+        resolve("human");
+    });
+
+}
+
+function checkMouseClick(solutionArray, mouseClicks){
+
+    if(mouseClicks.length < solutionArray.length){
+        return false;
+    }
+
+    for(var i = 0; i< solutionArray.length; i++){
+        var coordinates = solutionArray[i][1];
+        var click = mouseClicks[i];
+        console.log(i+ ":");
+        console.log(coordinates);
+        console.log(click);
+        if(click[0] < coordinates[0] || click[0] > coordinates[0]+pngSize){
+            return false;
+        }
+
+        if(click[1] < coordinates[1] || click[1] > coordinates[1]+pngSize){
+          return false;
+        }
+    }
+
+    return true;
+}
+
+
+function checkMouseMovement(mouseMovement){
+
+    return true;
+}
+
+module.exports = {
+    verify: verify
 }
